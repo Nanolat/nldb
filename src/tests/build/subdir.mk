5ba15b085b9cc94b4a0c16798cbcee644b706c30
Kangmo Kim <kangmo.kim@thankyousoft.com>
2013-03-28 07:50:36 +0900

    Changed makefiles

--- src/tests/build/subdir.mk
+++ src/tests/build/subdir.mk
@@ -20,7 +20,7 @@ CPP_DEPS += \
 %.o: ../%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I"/home/ladmin/tmp/nldb/src/include" -I"/home/ladmin/tmp/nldb/src/txbase" -O3 -Wall -c -fmessage-length=0 -std=c++0x -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I"/home/ladmin/nldb_work/nldb/src/include" -I"/home/ladmin/nldb_work/nldb/src/txbase" -O3 -Wall -c -fmessage-length=0 -std=c++0x -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
