6c17b73a02b322186d9f3eff00c9a091c4bac5b8
Kangmo Kim <kangmo.kim@thankyousoft.com>
2013-08-22 00:50:29 -0700

    Remove unnecessary makefiles

--- src/gtest/build/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lgtest -lnldb -lleveldb -ltokyocabinet -lxs -lpthread
-
--- src/nldb/build/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lleveldb -lxs -ltokyocabinet -lbz2 -lz -lpthread -ldl
-
