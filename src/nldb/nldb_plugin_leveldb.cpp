541c380fc8ddb443250988d471e81ba4c551c760
kangmo <kangmo@korbit.co.kr>
2014-04-26 11:22:04 +0900

    Get rid of AGPL license from the dual license. Now we have only Apache v2 license left.  IOW, Nanolat Database is under Apache v2

--- src/nldb/nldb_plugin_leveldb.cpp
+++ src/nldb/nldb_plugin_leveldb.cpp
@@ -2,38 +2,13 @@
  * (C) Copyright 2012, 2013 ThankyouSoft (http://ThankyouSoft.com/) and Nanolat(http://Nanolat.com).
  *                                                      Writen by Kangmo Kim ( kangmo@nanolat.com )
  *
- * ============
- * Dual License
- * ============
- * You can choose either free(AGPL v3) or commercial( Similar to Apache license v2 ).
- * 
- * ================================================================
- * Nanolat Database Commercial License for OEMs, ISVs, VARs and SPs
- * ================================================================
- * Nanolat provides its Nanolat Database server and Nanolat Client Libraries under a dual license model
- * designed to meet the development and distribution needs of both commercial distributors / service providers
- * (such as OEMs, ISVs, VARs, and SPs) and open source projects.
+ * =================
+ * Apache v2 License
+ * =================
+ * http://www.apache.org/licenses/LICENSE-2.0.html
  *
- * ==============================================================================
- * For OEMs, ISVs, VARs and Other Distributors of Commercial Applications and SPs
- * ==============================================================================
- * OEMs (Original Equipment Manufacturers), ISVs (Independent Software Vendors), VARs (Value Added Resellers)
- * and other distributors that combine and distribute commercially licensed software
- * with Nanolat Database software but do not wish to distribute the source code for the commercially licensed software
- * under version 3 of the GNU Affero General Public License (the "AGPL") must enter into a commercial license agreement with Nanolat.
- * SPs (Service Providers) also need to enter into a commercial license agreement with Nanolat otherwise they are under the AGPL v3.
- * The commercial license includes Apache v2 license, which does not require you to open source the project developed using Nanolat Database.
- * 
- * For start-up companies, Nanolat provides commercial license and technical support at small percentage of equity share.
- * Focus on your client side development and grow your business quicker by using Nanolat Database.
- * Send an email to support@nanolat.com for the details.
- *
- * =========================================================================
- * For Open Source Projects and Other Developers of Open Source Applications
- * =========================================================================
- * For developers of Free Open Source Software ("FOSS") applications under the GPL v3 or AGPL v3
- * that want to combine and distribute those FOSS applications with Nanolat Database software,
- * Nanolat Database open source software licensed under the AGPL v3 is the best option.
+ * Contributors : 
+ *   Kangmo Kim
  */
 
 #include <txbase/tx_assert.h>
--- src/txbase/txbase/tx_assert.h
+++ src/txbase/txbase/tx_assert.h
@@ -1,40 +1,14 @@
 /*
- * (C) Copyright 2012 ThankyouSoft (http://ThankyouSoft.com/) and Nanolat(http://Nanolat.com).
+ * (C) Copyright 2012, 2013 ThankyouSoft (http://ThankyouSoft.com/) and Nanolat(http://Nanolat.com).
+ *                                                      Writen by Kangmo Kim ( kangmo@nanolat.com )
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the NOSL(Nanolat Open Source License) v1.0.
- * 
- * Simple Description :
- * - If you are in USA, Canada, or Germany, it is *likely* that you get free LGPL v2.1.
- * - In other countries, you are *likely* under GPL v3, but you can by LGPL v2.1 from Nanolat Co., Ltd.
- * - For free software, you *definitely* get free LGPL v2.1 whereever you are.
- * 
- * Detailed Description :
- * - In the hope of seeing increasing open source contributions in countries 
- *   that do not have enough open source contributors, Nanolat Open Source License 
- *   provides free LGPL v2.1 only to companies and people of countries 
- *   that have at least three KudoRank 10 contributors in http://www.ohloh.net/people. 
- *   The list of countries are updated by the beginning of a year. 
- *   In 2013, these countries are USA, Canada, and Germany.
+ * =================
+ * Apache v2 License
+ * =================
+ * http://www.apache.org/licenses/LICENSE-2.0.html
  *
- * - For companies and people of other countries, you are under GPL v3.0, 
- *   but you can buy LGPL v2.1 license for a product from Nanolat Co., Ltd.
- *
- * - However, for non-commercial usage, you get free LGPL v2.1 whereever you are.
- *
- * - A company is of a country if the head of office is in the country.
- *
- * - A person is of a country if the nationality of the person is the country.
- *
- * - A country has a KudoRank 10 contributor 
- *   if the contributor's location is the country in http://www.ohloh.net/.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
- *
- * Contributors:
- *     Kangmo Kim
+ * Contributors : 
+ *   Kangmo Kim
  */
 
 #ifndef _O_TX_ASSERT_H_ 
